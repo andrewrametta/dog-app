66bb8a54da71352be5e136985f276437d9e4a082
Andrew Rametta <73006425+andrewrametta@users.noreply.github.com>
2020-11-14 12:18:45 -0500

    change index.js

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+function getDogImage(qty) {
+  // create a variable to store the form value
 
+  // add quantity at the end of the API link
+  fetch(`https://dog.ceo/api/breeds/image/random/${qty}`)
+    .then((response) => response.json())
+    .then((responseJson) => displayResults(responseJson));
+}
 
-function getDogImage() {
-    // create a variable to store the form value
-    const i = $('form #img-quantity').val();
-    // add quantity at the end of the API link 
-    fetch('https://dog.ceo/api/breeds/image/random/'+ i)
-      .then(response => response.json())
-      .then(responseJson => console.log(responseJson));
-  }
+function displayResults(results) {
+  console.log(results);
+  $(".images").html("");
+  results.message.map((imgurl) =>
+    $(".images").append(`<img src="${imgurl}" width="300"/>`)
+  );
+  //display the results section
+  $(".results").removeClass("hidden");
+}
 
-  
-  function watchForm() {
-    $('form').submit(event => {
-      event.preventDefault();
-      getDogImage();
-    });
-  }
-  
-  $(function() {
-    console.log('App ready!');
-    watchForm();
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    const qty = $("form #img-quantity").val();
+    getDogImage(qty);
   });
+}
+
+$(function () {
+  console.log("App ready!");
+  watchForm();
+});
